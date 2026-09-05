39ec9d83af4f5869d47c11a0b5b9c51a64b64d35
WPEssential <wordpress.essential@gmail.com>
2026-09-06 02:53:14 +0500

    P1-10: guard demo seeding in production

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use App\Providers\ProductionSafetyServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
+    AppServiceProvider::class,
+    ProductionSafetyServiceProvider::class,
 ];
