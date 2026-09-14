b069c3bb51016f24eed95d1d2ac95735a74db090
WPEssential <wordpress.essential@gmail.com>
2026-09-15 03:22:20 +0500

    security: sanitize auth continuation before router boot

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -4,9 +4,19 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import {CartProvider} from './platform/cart';
 import {AuthProvider} from './platform/auth';
+import {sanitizeRedirectSearch} from './platform/navigation';
 import {UXProvider} from './components/UXProvider';
 import './styles.scss';
 import './accessibility.scss';
+
+const safeSearch = sanitizeRedirectSearch(window.location.search);
+if (safeSearch !== window.location.search) {
+  window.history.replaceState(
+    window.history.state,
+    '',
+    `${window.location.pathname}${safeSearch}${window.location.hash}`,
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
