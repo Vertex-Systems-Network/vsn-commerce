91e9dda4d0a85672437cfefcaddfab8b8ff93e3d
WPEssential <wordpress.essential@gmail.com>
2026-09-15 02:38:58 +0500

    style(auth): remove unused controller import

--- app/Http/Controllers/Api/V1/SocialAuthController.php
+++ app/Http/Controllers/Api/V1/SocialAuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserResource;
 use App\Models\SocialAccount;
 use App\Models\User;
 use App\Support\SafeRedirect;
